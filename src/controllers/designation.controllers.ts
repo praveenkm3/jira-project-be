3269aced53fb08aed07ff163557250202406ffb2
Praveen Kumar Mekala <praveen@CELSYSBLR000334.local>
2026-08-22 19:37:32 +0530

    designations can be edit and delete by admin

--- src/controllers/designation.controllers.ts
+++ src/controllers/designation.controllers.ts
@@ -1,5 +1,5 @@
 import type { Request, Response, NextFunction } from "express";
-import { addDesignationService } from "../services/designation_services.ts";
+import { addDesignationService,updateDesignationService,deleteDesignationService} from "../services/designation_services.ts";
 
 export async function addDesignation(
   req: Request,
@@ -12,6 +12,50 @@ export async function addDesignation(
     const response = await addDesignationService(
       user_id as string,
       designation,
+    );
+    return res.status(201).json(response);
+  } catch (error) {
+    next(error);
+  }
+}
+export async function updateDesignation(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try { 
+    const user_role=req.user?.role;
+    if(!user_role || user_role !== 'admin'){
+      return {
+        message:"Only Admin can update designations"
+      }
+    }
+    const { designation_name = "" } = req.body;
+    const{designation_id=""}=req.params;
+    const response = await updateDesignationService( 
+      designation_id as string,
+      designation_name as string,
+    );
+    return res.status(201).json(response);
+  } catch (error) {
+    next(error);
+  }
+}
+export async function deleteDesignation(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try { 
+    const user_role=req.user?.role;
+    if(!user_role || user_role !== 'admin'){
+      return {
+        message:"Only Admin can delete designations"
+      }
+    } 
+    const{designation_id=""}=req.params;
+    const response = await deleteDesignationService( 
+      designation_id as string
     );
     return res.status(201).json(response);
   } catch (error) {
--- src/services/designation_services.ts
+++ src/services/designation_services.ts
@@ -47,3 +47,80 @@ export async function addDesignationService(
     throw new AppError(500, "Unable to add designation");
   }
 }
+export async function updateDesignationService(
+  designation_id: string,
+  new_name: string,
+) {
+  try {
+    if (!new_name.trim()) {
+      throw new AppError(409, "Designation name required");
+    }
+    const existingDesignation = await designationRepository.findOne({
+      where: {
+        designation_name: new_name.trim(),
+      },
+    });
+    if (existingDesignation) {
+      throw new AppError(409, "Designation on that name already exists");
+    }
+
+    const query = await designationRepository.update(designation_id, {
+      designation_name: new_name.trim(),
+    });
+    if (query.affected === 1) {
+      return {
+        success: true,
+        message: "Designation updated successfully",
+      };
+    }
+
+    return {
+      success: false,
+      message: "Designation not found",
+    };
+  } catch (error) {
+    if (error instanceof AppError) {
+      throw error;
+    }
+
+    throw new AppError(500, "Unable to update designation");
+  }
+}
+export async function deleteDesignationService(designation_id: string) {
+  try {
+    if (!designation_id) {
+      throw new AppError(409, "Designation Id required");
+    }
+    const check1 = await usersRepo.find({
+      where: {
+        designation: {
+          designation_id: designation_id,
+        },
+      },
+    });
+    if (check1.length > 0) {
+      return {
+        success: false,
+        message: "Designation Currently in used",
+      };
+    }
+    const query = await designationRepository.delete(designation_id);
+    if (query.affected === 1) {
+      return {
+        success: true,
+        message: "Designation deleted successfully",
+      };
+    }
+
+    return {
+      success: false,
+      message: "Designation not found",
+    };
+  } catch (error) {
+    if (error instanceof AppError) {
+      throw error;
+    }
+
+    throw new AppError(500, "Unable to delete designation");
+  }
+}
